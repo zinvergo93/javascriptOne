8f3c427ce9ebdc94230e3f39595cd8d36615e2cb
zinvergo57 <zachary.coding@gmail.com>
2020-09-30 16:53:44 -0500

    commit changes

--- conditionals.js
+++ conditionals.js
@@ -75,6 +75,7 @@
 //     if (age < 16) {
 //         console.log('You are old enough to get a permit');
 //         console.log('You are not old enough to get a license');
+//         return true
 //     }
 //     else {
 //         console.log('You are old enough to get a license and do not need a permit')
@@ -82,3 +83,71 @@
 // }
 
 // console.log(kid())
+
+// ************SWITCH/CASE STATEMENTS******************
+
+// var dataPoint = {};
+
+// switch (typeof dataPoint) {
+//   case "string":
+//     console.log("It's a string");
+//     break;
+//   case "number":
+//     console.log("It's a number");
+//     break;
+//   case "boolean":
+//     console.log("It's a boolean");
+//     break;
+//   default:
+//     console.log('No matches');
+// }
+
+
+// *************TERNARY OPERATOR****************
+
+function ageVerification(age) {
+    // if (age > 25) {
+    //   console.log('can rent a car');
+    // } else {
+    //   console.log("can't rent a car");
+    // }
+  
+    return age > 25 ? "can rent a car" : "can't rent a car";
+  }
+  
+  ageVerification(30); //?
+  ageVerification(10); //?
+  
+  function adminControls(user) {
+    // if (user) {
+    //   if (user.admin) {
+    //     return 'showing admin controls...';
+    //   } else {
+    //     return 'You need to be an admin';
+    //   }
+    // } else {
+    //   return 'You need to be logged in';
+    // }
+  
+    return user
+      ? user.admin ? "showing admin controls" : "You need to be an admin"
+      : "you need to be logged in";
+  }
+  
+  const userOne = {
+    name: "Kristine",
+    admin: true
+  };
+  
+  adminControls(userOne); //?
+  
+  const userTwo = null;
+  
+  adminControls(userTwo); //?
+  
+  const userThree = {
+    name: "Tiffany",
+    admin: false
+  };
+  
+  adminControls(userThree); //? 
